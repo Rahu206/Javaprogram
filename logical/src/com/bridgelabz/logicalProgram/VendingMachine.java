6c3f5f6366ee311e47870504ea5cf3729fd9d6bd
Rahu206 <81007551+Rahu206@users.noreply.github.com>
2021-07-08 01:31:25 +0530

    Update VendingMachine.java

--- logical/src/com/bridgelabz/logicalProgram/VendingMachine.java
+++ logical/src/com/bridgelabz/logicalProgram/VendingMachine.java
@@ -1,44 +1,35 @@
 package com.bridgelabz.logicalProgram;
-import java.util.Scanner;
+import java.util.*;
 
-public class VendingMachine {
-
-     /** static variable I is declared to change notes and Static variable TOTAL is declared to count total notes */
-
-	static int I, TOTAL;
-
-	/** Static array to store values of notes */
-
-	static int[] NOTES = { 1000, 500, 100, 50, 10, 5, 2, 1 };
-
-	/*** Function to find the notes and print the output **/
-
-    	static void notes(int value) {
-	   	if (value / NOTES[I] != 0) {
-	 		TOTAL += (value / NOTES[I]);
-			System.out.println(NOTES[I] + "rs notes :" + value / NOTES[I]);
-			value = value % NOTES[I];
-	      	}
-		   I++;
-		   if (value == 0) {
-			System.out.println("total notes :" + TOTAL);
-			return;
-	    	  }
-
-		notes(value);
-
-	       }
-
-	/*** main method to test the class ***/
-	public static void main(String[] args) {
-		try {
-			Scanner s = new Scanner(System.in);
-			System.out.println("enter amoumt");
-			int value = s.nextInt();
-			notes(value);
-			s.close();
-	       	} catch (Exception e) {
-			System.out.println("enter value in integer");
-		}
+	public class vendingmachine{
+	    public static void countCurrency(int amount)
+	    {
+	        int[] notes = new int[]{ 1000, 500, 200, 100, 50, 20, 10, 5, 1 };
+	        int[] noteCounter = new int[9];
+	      
+	        for (int i = 0; i < 9; i++) {
+	            if (amount >= notes[i]) {
+	                noteCounter[i] = amount / notes[i];
+	                amount = amount - noteCounter[i] * notes[i];
+	            }
+	        }
+	      
+	        System.out.println("Currency Count ->");
+	        for (int i = 0; i < 9; i++) {
+	            if (noteCounter[i] != 0) {
+	                System.out.println(notes[i] + " : "
+	                    + noteCounter[i]);
+	            }
+	        }
+	    }
+	     
+	    public static void main(String argc[]){
+	    	Scanner sc = new Scanner(System.in);
+	    	System.out.println("Enter the amount you have: ");
+	        int amount = sc.nextInt();
+	        countCurrency(amount);
+	    }
 	}
-}
+
+
+
