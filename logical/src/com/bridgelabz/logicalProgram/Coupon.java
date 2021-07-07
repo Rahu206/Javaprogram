30c106dcf848415b5bd96e6758ddb51a404c53d7
Rahu206 <81007551+Rahu206@users.noreply.github.com>
2021-07-08 00:59:33 +0530

    Update Coupon.java

--- logical/src/com/bridgelabz/logicalProgram/Coupon.java
+++ logical/src/com/bridgelabz/logicalProgram/Coupon.java
@@ -1,30 +1,34 @@
 package com.bridgelabz.logicalProgram;
 import java.util.Scanner;
 public class Coupon {
-	public static void main(String[] args) {
-		/*
-		 * char array to get characters for coupen
-		 */
-		char[] chars = "abcdefghijklmnop".toCharArray();
-		/*
-		 * length of coupen
-		 */
-		int max = 100000000;
-		/*
-		 * random value
-		 */
-		double d = Math.random();
-		int random = (int) (d * max);
-		String sb = "";
-		System.out.println("random double " + d + " random at start " + random);
-
-		while (random > 0) {
-			sb = sb + (chars[random % chars.length]);
-			random /= chars.length;
-			System.out.println(random + "  " + random % chars.length + " sb is " + sb);
-		}
-
-		String couponCode = sb.toString();
-		System.out.println("Coupon Code: " + couponCode);
+	public static int generateRandomNumber() {
+	//random function to generate the distinct number
+	return 0;	
 	}
+	
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter the N : ");
+		
+		int n = scanner.nextInt();
+		int countDistinct =0;
+		int[]couponArray = new int [n];
+		while(n>=countDistinct) {
+			int randomNumber = generateRandomNumber();
+			for(int i=0;i<n;i++) {
+				if(randomNumber!= couponArray[i]) {
+					couponArray[i]=randomNumber;
+					countDistinct++;
+				}
+				
+			}
+		}
+		
+	}
+	
+	
 }
+
+	
+
